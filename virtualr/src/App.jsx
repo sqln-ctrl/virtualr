d5d715f33e2265d8a5a253bd66d5629b4f82409e
sqln-ctrl <sqlnnqvi@gmail.com>
2026-03-18 14:21:55 +0500

    Design Features Section

--- virtualr/src/App.jsx
+++ virtualr/src/App.jsx
@@ -2,12 +2,14 @@ import './index.css';
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import Features from './components/Features.jsx';
 function App () {
   return (
      <>
          <Navbar/>
          <div className='max-w-7xl mx-auto pt-20 px-6'>
              <Hero/>
+             <Features/>
           </div> 
 
      </>
